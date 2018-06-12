faee187d2b7883c9321f5764859a675a4c24c070
Godfrey Zubiaga <godfreyzubiaga@gmail.com>
2018-06-12 15:15:26 +0800

    feat: export new components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 export { H1, H2, H3, H4, P } from './components/typography';
-
 export { default as Button } from './components/button';
+export { default as Card } from './components/card';
+export { default as Input } from './components/input';
+export { default as VerticallyCenteredDiv } from './components/vcd';
+export { default as Line } from './components/line';
