f46eb3970b4cc289fa0604f20284243d0aecb4f0
Godfrey Zubiaga <godfreyzubiaga@gmail.com>
2018-06-12 15:13:09 +0800

    feat: add margin props

--- src/components/button.js
+++ src/components/button.js
@@ -5,10 +5,11 @@ export default styled.button`
   color: ${props => (props.primary ? 'white' : '1px solid black')};
   background: ${props => (props.primary ? '#1e90ff' : 'none')};
   border-radius: 3px;
-  padding: ${props => (props.small ? '7px 12px' : '6px 20px')};
-  font-size: ${props => (props.small ? '0.8em' : '0.9')};
+  padding: ${props => (props.small ? '7px 12px' : '6px 25px')};
+  font-size: ${props => (props.small ? '0.8em' : '0.9em')};
   cursor: pointer;
   transition-duration: 0.3s;
+  margin: ${props => (props.margin ? props.margin : '20px auto')};
 
   &:hover {
     transition-duration: 0.3s;
